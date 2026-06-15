df0e89a227ac1850107b885bdfb541ff9845316f
Your Name <you@example.com>
2026-06-15 11:38:21 +0500

    new changes quick

--- quicksort/e-commerce/quick.py
+++ quicksort/e-commerce/quick.py
@@ -1,12 +1,35 @@
+# Ручка TEST
 def test():
     print("Test OK")
 
-'''
-Схема Ломуто
-'''
 
-def quicksort(arr, field="id", reverse=False):
-    quicksort_lomuto(arr, 0, len(arr) - 1, field, reverse)
+def build_rules(fields, reverse):
+    pass
+
+def make_comparator(rules):
+    def compare(a, b):
+        for field, reverse in rules:
+            av = a[field]
+            bv = a[field]
+            if (av < bv):
+                return 1 if reverse else -1
+            if (av > bv):
+                return -1 if reverse else 1
+        return 0
+    return compare
+    
+
+# Быстрая сортировка (Схема Ломуто)
+def partition_lomuto(arr, low, high, field, reverse):
+    pivot = arr[high] # Опорный элемент - последний
+    i = low # Указатель на место для меньшего элемента
+    
+    for j in range(low, high):
+        if arr[j][field] > pivot[field]:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[i], arr[high] = arr[high], arr[i]
+    return i
 
 def quicksort_lomuto(arr, low, high, field, reverse):
     if low < high:
@@ -15,32 +38,5 @@ def quicksort_lomuto(arr, low, high, field, reverse):
         quicksort_lomuto(arr, low, pivot_index - 1, field, reverse)
         quicksort_lomuto(arr, pivot_index + 1, high, field, reverse)
 
-def partition_lomuto(arr, low, high, field, reverse):
-    pivot = arr[high] # Опорный элемент - последний
-    i = low # Указатель на место для меньшего элемента
-    
-    if (not reverse):
-        for j in range(low, high):
-            if arr[j][field] <= pivot[field]:
-                arr[i], arr[j] = arr[j], arr[i]
-                i += 1
-    else:
-        for j in range(low, high):
-            if arr[j][field] > pivot[field]:
-                arr[i], arr[j] = arr[j], arr[i]
-                i += 1
-
-    arr[i], arr[high] = arr[high], arr[i]
-    return i
-
-# def quicksort(arr, field, reverse=False):
-#     if (not reverse):
-#         print("Sort ASC")
-#     else:
-#         print("Sort DESC")
-
-# try:
-#     quicksort([], "", False)
-#     print("ok.")
-# except TypeError:
-#     print("error")
+def quicksort(arr, field="id", reverse=False):
+    quicksort_lomuto(arr, 0, len(arr) - 1, field, reverse)
